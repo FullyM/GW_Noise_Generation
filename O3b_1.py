b14c149c6a03d29de9e6af3bcad7957793648849
Oli <oliver.boehler99@gmail.com>
2023-02-27 20:18:47 +0100

    Finished sample processing function and made a script that executes the function to download the first set of data

---
+++ O3b_1.py
@@ -0,0 +1,3 @@
+from sample_processing import sample_processing
+
+sample_processing('H', 1256665088, 1256677375, 1, 'Spectrograms', q_range=(8, 32))
--- sample_processing.py
+++ sample_processing.py
@@ -3,11 +3,31 @@ from gwpy.timeseries import TimeSeries
 from gwdatafind import find_urls
 from Spectrogram_Plots import plot_q
 
+
 def sample_processing(detector, start_time, end_time, sample_duration, dir_name, **q_kws):
+    '''
+    Composite function that downloads data of a specified detector with specified start and end times from GWOSC using
+    CVMFS, and then divides it into equal siced chunks of data with duration sample_duration. These data chunks
+    are then Q-Transformed using the plot_q function, which also plots the spectrogram, without plot axis or borders
+    by default. Used to create and save samples for deep learning training.
+    :param detector: string, mandatory, takes either 'H', 'L' or 'V' and refers to the corresponding the detector
+    :param start_time: int, mandatory, specifies the start time of the dataset to be downloaded, the specified timeframe
+                        between start and end time must have no gaps in the data, otherwise will give back an error
+    :param end_time: int, mandatory, specified the end time of the dataset to be downloaded, the specified timeframe
+                     between start and end times must have no gaps in the data, otherwise will return an error
+    :param sample_duration: float, mandatory, duration of the individual samples to be produced
+    :param dir_name: string, mandatory, name of the directory in which the produced spectrograms should be saved,
+                     needs to already exist, if not in the current directory must give the path without starting and end
+                     '/'
+    :param q_kws: keyword arguments to be passed to the plot_q function, for possible arguments look at
+                  Spectrogram_Plots.py, where the function is implemented
+    :return: Returns nothing, but saves the spectrograms to the specified directory, named 'sample_n' where n is the
+             number of the sample
+    '''
 
     file_url1 = find_urls(f'{detector}', f'{detector}1_GWOSC_O3b_4KHZ_R1', start_time, end_time,
                           host='datafind.gw-openscience.org', on_gaps='error')
-    time_series1 = TimeSeries.read(file_url1, 'H1:GWOSC-4KHZ_R1_STRAIN', start=start_time, end=end_time)
+    time_series1 = TimeSeries.read(file_url1, f'{detector}1:GWOSC-4KHZ_R1_STRAIN', start=start_time, end=end_time)
 
     samples = np.array([])
     sample_length = sample_duration*time_series1.sample_rate.value
