3360233e066e6bddd341b97efa249af91ec23ea1
Oli <oliver.boehler99@gmail.com>
2023-03-31 21:02:18 +0200

    Replaced old manual logging with tensorboard and implemented learning rate scheduler

--- Noise_Generation.py
+++ Noise_Generation.py
@@ -1,8 +1,6 @@
 import torchvision.transforms
-
 import Noise_VAE
 import torch
-import matplotlib.pyplot as plt
 from Noise_VAE import train, val
 from Data_Setup import construct_dataloaders
 import torch.optim as optim
@@ -21,51 +19,17 @@ writer = SummaryWriter()
 
 model = Noise_VAE.ConvVAE().to(device)
 
-learning_rate = 0.001
+learning_rate = 0.01
 epochs = 50
 optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-
-grid_images = []
-
-train_loss = []
-val_loss = []
+scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.1)
 
 for epoch in range(1, epochs+1):
     print(f'Epoch {epoch} of {epochs}')
     epoch_train_loss = train(model, train_loader, optimizer, epoch)
     epoch_val_loss, recon_images, original_images = val(model, val_loader)
-    writer.add_scalar('Loss/train', epoch_train_loss)
-    writer.add_scalar('Loss/validation', epoch_val_loss)
+    writer.add_scalar('Loss/train', epoch_train_loss, epoch)
+    writer.add_scalar('Loss/validation', epoch_val_loss, epoch)
     writer.add_images('Originals', original_images.cpu()[:20], epoch)
     writer.add_images('Reconstructions', recon_images.cpu()[:20], epoch)
-    train_loss.append(epoch_train_loss)
-    val_loss.append(epoch_val_loss)
-
-
-fig = plt.figure(figsize=(20, 10))
-recon_images = recon_images.cpu().numpy().transpose((0, 2, 3, 1))
-for i in range(40):
-  plt.subplot(5,8,i+1)
-  plt.tight_layout()
-  plt.imshow(recon_images[i], interpolation='none')
-  plt.xticks([])
-  plt.yticks([])
-#plt.savefig('./ConvVAE/MNIST_recon')
-
-
-fig = plt.figure(figsize=(20, 10))
-original_images = original_images.cpu().numpy().transpose((0, 2, 3, 1))
-for i in range(40):
-    plt.subplot(5,8,i+1)
-    plt.tight_layout()
-    plt.imshow(original_images[i], interpolation='none')
-    # plt.title("Ground Truth: {}".format(example_targets[i]))
-    plt.xticks([])
-    plt.yticks([])
-#plt.savefig('./ConvVAE/MNIST_originals')
-
-fig1 = plt.figure()
-plt.plot(train_loss)
-plt.plot(val_loss, c='orange')
-plt.xlabel('epoch')
-#plt.savefig('./ConvVAE/Loss_plot')
+    scheduler.step()
